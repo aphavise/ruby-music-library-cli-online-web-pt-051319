4fd3aa0417c8f3875b85337c946be4c49b673042
Andrew Phaviseth <andrewphaviseth@gmail.com>
2019-06-27 05:57:51 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -7,7 +7,7 @@ class MusicLibraryController
   
   def call 
     input = ''
-    input != 'exit'
+    while input != 'exit'
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -21,7 +21,7 @@ class MusicLibraryController
       
       input = gets.strip
     end
-  end 
+  end
   
    extend Concerns::Findable
 
