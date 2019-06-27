6b982bb7a4edb02227e9fb9861d9c900ca3df349
Andrew Phaviseth <andrewphaviseth@gmail.com>
2019-06-27 06:14:21 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -71,7 +71,7 @@ class MusicLibraryController
   end
   
   def list_artists
-    Artist.all.sort_by(&:name).each.with_index(1) do |artist,idx|
+    Artist.all.sort_by(&:name).each.with_index(0) do |artist,idx|
       puts "#{idx}. #{artist.name}"
       binding.pry
     end
